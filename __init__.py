2c538018aa7b0c9c2e86ede1b119af950945b79a
Rohit Tiwari <rohit.beawar@gmail.com>
2020-05-21 13:41:19 +0530

    DB uri updated

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ from flask_restplus import Api
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:////DB/userresttest.db'
+app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:////home/rohit/Learn-FLask-python/DB/userresttest.db'
 db = SQLAlchemy(app)
 api=Api(app)
